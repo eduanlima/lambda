15f35d07d28513360c4fe9691c4e027f8ed10637
eduanlima <eduandev@gmail.com>
2022-03-01 22:29:21 -0300

    Fifth example.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -11,23 +10,22 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD case", 80.90));
-		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-		list.removeIf(pred);	
+
+		list.removeIf(p -> p.getPrice() <= 100);
 		print(list);
-		
+
 	}
-	
+
 	public static void print(List<Product> list) {
 		for (Product p : list) {
 			System.out.println(p);
 		}
-		
+
 		System.out.println();
-	} 
+	}
 }
